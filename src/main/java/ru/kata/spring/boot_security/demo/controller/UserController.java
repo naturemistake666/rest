776111c4c1c3d3703371f6fd3514e341dd3278cc
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-06-23 21:15:18 +0300

    Rest Controller Add

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,30 +1,22 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
-   private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping(value = "login")
     public String loginPage() {
         return "login";
     }
-    @GetMapping("/user")
-    public String getUser(Model model, Principal principal) {
-        model.addAttribute("user", userService.findByEmail(principal.getName()));
-        return "user";
-    }
+   @GetMapping("/")
+   public String startPage() {
 
+       return "index";
+    }
 }
